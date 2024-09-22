c7a0d71eb98d52eee508c291ac14835a24a8fa76
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-22 12:41:23 +0100

    Create ft_boolean.h

---
+++ docs/projects/C08/ex01/ft_boolean.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 20:52:19 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/09/12 13:49:39 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define EVEN(nbr) (nbr % 2 == 0)
+
+typedef int	t_bool;
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+#endif
