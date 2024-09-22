ffab7b2084fd2c49e6aaebf9b7951344196c7ed6
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-22 12:33:26 +0100

    Create ft_strdup.c

---
+++ docs/projects/C07/ex00/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:49:02 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/09/07 13:27:19 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*p;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(src);
+	p = (char *)malloc(sizeof(char) * (len + 1));
+	if (!p)
+		return (NULL);
+	while (src[i])
+	{
+		p[i] = src[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
