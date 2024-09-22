c3868d44129dcaab7862c39b37db1758633685c4
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-22 11:28:06 +0100

    Create ft_print_comb2.c

---
+++ docs/projects/C00/ex06/ft_print_comb2.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 21:18:14 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/08/27 21:21:45 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + 48);
+			ft_putchar(a % 10 + 48);
+			ft_putchar(' ');
+			ft_putchar(b / 10 + 48);
+			ft_putchar(b % 10 + 48);
+			if (a != 98)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}
+}
