13af06ddf94b557fd584e0c2e4f6053672555e08
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-22 11:59:45 +0100

    Create ft_strstr.c

---
+++ docs/projects/C03/ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 09:03:12 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/09/04 15:36:10 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	if (!to_find[0])
+		return (str);
+	while (str[i])
+	{
+		j = 0;
+		while (str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (str + i);
+			if (str[i + j + 1] == '\0')
+				return ((char *) 0);
+			j++;
+		}
+		i++;
+	}
+	return ((char *) 0);
+}
