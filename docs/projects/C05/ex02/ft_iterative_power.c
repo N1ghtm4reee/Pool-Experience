88f1471f0cc078cd3633d95217169f0a013e7a6c
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-22 12:07:23 +0100

    Create ft_iterative_power.c

---
+++ docs/projects/C05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 20:49:51 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/09/04 14:11:01 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0 && nb == 0)
+		return (1);
+	while (power > 0)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
