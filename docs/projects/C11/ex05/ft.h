1d863f1a2b5323bde581c364a12e3c7e9e1a64dc
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-23 13:55:44 +0100

    Create ft.h

---
+++ docs/projects/C11/ex05/ft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 11:07:45 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/09/18 13:13:30 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+int		ft_atoi(char *str);
+void	ft_div(int nb1, int nb2);
+void	ft_mod(int nb1, int nb2);
+void	ft_mult(int nb1, int nb2);
+void	ft_sub(int nb1, int nb2);
+void	ft_sum(int nb1, int nb2);
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
+
+#endif
