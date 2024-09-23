0b986b438b53fc4a58052d3209c1147645bf0ba2
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-23 13:53:07 +0100

    Update ft_is_sort.c

--- docs/projects/C11/ex04/ft_is_sort.c
+++ docs/projects/C11/ex04/ft_is_sort.c
@@ -6,20 +6,33 @@
 /*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 17:22:23 by aakhrif           #+#    #+#             */
-/*   Updated: 2024/09/12 18:30:57 by aakhrif          ###   ########.fr       */
+/*   Updated: 2024/09/17 19:02:51 by aakhrif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
+	int	front;
+	int	back;
 	int	i;
 
 	i = 0;
+	front = 0;
+	back = 0;
 	while (i < length - 1)
 	{
-		if ((*f)(tab[i], tab[i + 1]) >= 0)
-			return (0);
+		if ((*f)(tab[i], tab[i + 1]) < 0)
+			front++;
+		else if ((*f)(tab[i], tab[i + 1]) > 0)
+			back++;
+		else
+		{
+			front++;
+			back++;
+		}
 		i++;
 	}
-	return (1);
+	if (back == i || front == i)
+		return (1);
+	return (0);
 }
