4ff92d7c4ac42605776074297b0d9d5b65c99ce1
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-23 14:02:02 +0100

    Create ft_strlcpy.c

---
+++ docs/projects/C02/ex10/ft_strlcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhrif <aakhrif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 16:29:01 by aakhrif           #+#    #+#             */
+/*   Updated: 2024/09/02 16:30:31 by aakhrif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	slen;
+
+	i = 0;
+	slen = ft_strlen(src);
+	if (size == 0)
+		return (slen);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (slen);
+}
