5a5d4d1b2e8a5124d385f433aec1e678264da12f
N1ghtm4reee <ayoube_akhrif2015@outlook.fr>
2024-09-29 11:12:35 +0100

    Create ft_utils.c

---
+++ docs/projects/bsq/srcs/ft_utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yalazrek <yalazrek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 12:00:19 by yalazrek          #+#    #+#             */
+/*   Updated: 2024/09/18 20:58:58 by yalazrek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+int	ft_is_number(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
